edabe9e537f3855ba99557952603664b670e787b
Gavin Chan <gavinkelsier2018@gmail.com>
2023-06-13 23:52:50 -0400

    Alter: nvmrc

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/resume-vue/',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Gavin Chan'
+        return args
+      })
+  }
 })
